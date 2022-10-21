1cc6b6d92aef6f02ae4374a26ae29026cf14692d
Selim Fazylov <selim.fazylov1@gmail.com>
2022-10-22 00:04:29 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+function App() {
+  const canvas = document.querySelector('canvas');
+  const ctx = canvas.getContext('2d');
+
+  ctx.beginPath();
+  ctx.moveTo(50, 50);
+  ctx.lineCap = 'round';
+  ctx.lineTo(200, 200);
+  ctx.stroke();
+
+  return (
+    <div className="App">
+      <canvas></canvas>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Canvas/Canvas.jsx
@@ -0,0 +1,7 @@
+function Canvas() {
+
+}
+
+module.exports = {
+    
+}
