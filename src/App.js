c520e1d5844a502e9741a010378d5fb1e631a6ad
Selim Fazylov <selim.fazylov1@gmail.com>
2022-10-22 00:09:40 +0300

    new components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
+import Canvas from "./Canvas/Canvas";
+
 
 function App() {
-  const canvas = document.querySelector('canvas');
-  const ctx = canvas.getContext('2d');
-
-  ctx.beginPath();
-  ctx.moveTo(50, 50);
-  ctx.lineCap = 'round';
-  ctx.lineTo(200, 200);
-  ctx.stroke();
+  
 
   return (
     <div className="App">
-      <canvas></canvas>
+      <Canvas/>
     </div>
   );
 }
--- src/Canvas/Canvas.jsx
+++ src/Canvas/Canvas.jsx
@@ -1,7 +1,18 @@
 function Canvas() {
+    const canvas = document.querySelector('canvas');
+    const ctx = canvas.getContext('2d');
 
+    ctx.beginPath();
+    ctx.moveTo(50, 50);
+    ctx.lineCap = 'round';
+    ctx.lineTo(200, 200);
+    ctx.stroke();
+
+    return (
+    <div >
+        <canvas></canvas>
+    </div>
+    );
 }
 
-module.exports = {
-    
-}
+export default Canvas;
