f260e366260dec18b76e6dd62dad9ab4812043be
Selim Fazylov <selim.fazylov1@gmail.com>
2022-10-27 00:19:54 +0300

    onclick function

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
 import CollapsBtn from "./CollapseBtn/CollapseBtn";
 
 const canvas = document.querySelector("canvas");
-console.log(canvas);
-
 
 const wrapper = document.querySelector('.wrapper');
 const stock = document.createElement("p");
 stock.textContent = 'This browser is not support this file';
 
-if (canvas.getContext) {
-  const ctx = canvas.getContext("2d");
+const ctx = canvas.getContext("2d");
+console.log(canvas)
 
+if (canvas.getContext) {
+  
   
 
   const line = new Path2D();
-  console.log(line)
-    line.moveTo(125, 35);
-    line.lineTo(200, 200);
-  
+  line.moveTo(125, 35);
+  line.lineTo(200, 200);
   
   const line2 = new Path2D();
   line2.moveTo(150, 150);
   line2.lineTo(300, 400);
 
-  ctx.stroke(line);
-  ctx.stroke(line2)
+  // ctx.stroke(line);
+  // ctx.stroke(line2)
   
   // ctx.beginPath();
   // ctx.moveTo(50, 20);
@@ -34,19 +32,32 @@ if (canvas.getContext) {
   wrapper.append(stock);
 }
 
-function startingDrawing() {
+function startDrawing(elem, e) {
+  const [x, y] = getMousePosition(elem, e);
+  
   const ctx = canvas.getContext("2d");
   ctx.beginPath();
-  ctx.moveTo(200, 200);
+  ctx.moveTo(x, y);
   ctx.lineWidth = 1;
   ctx.lineCap = 'round';
   ctx.lineTo(300, 300);
   ctx.stroke();
 }
 
+function getMousePosition(canvas, click) {
+  let rect = canvas.getBoundingClientRect();
+  
+  const x = click.clientX - rect.left;
+  const y = click.clientY - rect.top;
+  
+  return [x, y];
+}
+
 // const startX = 200;
 // const startY = 200;
-canvas.addEventListener('click', startingDrawing);
+canvas.addEventListener('click', (e) => {
+  startDrawing(canvas, e);
+});
 
 
 function App() {
