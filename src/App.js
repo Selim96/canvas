4915636708dfaecea5cfd48ca0690ca62249c96e
Selim Fazylov <selim.fazylov1@gmail.com>
2022-10-28 00:28:12 +0300

    onclick events

--- src/App.js
+++ src/App.js
@@ -4,65 +4,109 @@ const canvas = document.querySelector("canvas");
 
 const wrapper = document.querySelector('.wrapper');
 const stock = document.createElement("p");
-stock.textContent = 'This browser is not support this file';
+stock.textContent = 'This browser is not support canvas elements';
 
-const ctx = canvas.getContext("2d");
 console.log(canvas)
 
-if (canvas.getContext) {
+// if (canvas.getContext) {
   
   
+//   const line = new Path2D();
+//   line.moveTo(125, 35);
+//   line.lineTo(200, 200);
+  
+//   const line2 = new Path2D();
+//   line2.moveTo(150, 150);
+//   line2.lineTo(300, 400);
 
-  const line = new Path2D();
-  line.moveTo(125, 35);
-  line.lineTo(200, 200);
+//   // ctx.stroke(line);
+//   // ctx.stroke(line2)
   
-  const line2 = new Path2D();
-  line2.moveTo(150, 150);
-  line2.lineTo(300, 400);
+//   ctx.beginPath();
+//   ctx.moveTo(50, 20);
+//   ctx.lineTo(100, 50);
+//   ctx.stroke();
+// } else {
+//   wrapper.append(stock);
+// }
 
-  // ctx.stroke(line);
-  // ctx.stroke(line2)
-  
-  // ctx.beginPath();
-  // ctx.moveTo(50, 20);
-  // ctx.lineTo(100, 50);
-  // ctx.stroke();
-} else {
-  wrapper.append(stock);
-}
+const ctx = canvas.getContext("2d");
 
-function startDrawing(elem, e) {
-  const [x, y] = getMousePosition(elem, e);
-  
-  const ctx = canvas.getContext("2d");
+function drawLine(ctx, line) {
+  const {start, end } = line;
   ctx.beginPath();
-  ctx.moveTo(x, y);
-  ctx.lineWidth = 1;
-  ctx.lineCap = 'round';
-  ctx.lineTo(300, 300);
+  ctx.moveTo(start.x, start.y);
+  ctx.lineWidth = 3;
+  ctx.lineTo(end.x, end.y);
   ctx.stroke();
-}
+};
 
-function getMousePosition(canvas, click) {
+let isStartDrawing = false;
+let mouseStartPos = null;
+
+
+function getMousePosition(click) {
   let rect = canvas.getBoundingClientRect();
   
   const x = click.clientX - rect.left;
   const y = click.clientY - rect.top;
-  
-  return [x, y];
+  return {x, y};
 }
 
-// const startX = 200;
-// const startY = 200;
-canvas.addEventListener('click', (e) => {
-  startDrawing(canvas, e);
+canvas.addEventListener('mousedown', (e) => {
+  e.preventDefault();
+  
+  mouseStartPos = getMousePosition(e);
+  
+  const line = {
+    start: mouseStartPos,
+    end: mouseStartPos
+  };
+
+  if (e.button === 0) {
+    isStartDrawing = !isStartDrawing;
+    if (isStartDrawing) {
+      drawLine(ctx, line);
+    }
+  } if (e.button === 2) {
+    isStartDrawing = false;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  } if (e.button === 1) {
+    isStartDrawing = false;
+  }
 });
+
+canvas.addEventListener('mousemove', (e) => {
+  if (isStartDrawing) {
+    let line = {
+      start: mouseStartPos,
+      end: getMousePosition(e),
+    }
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    drawLine(ctx, line);
+  }
+});
+
+canvas.oncontextmenu = ((e) => {
+  return false;
+});
+// canvas.oncontextmenu = (function (e) {
+  
+//   if (isRMBActive) {
+//     ctx.clearRect(0, 0, canvas.width, canvas.height);
+//   }
+//   // isRMBActive = false;
+//   return false;
+// });
+
+
+
+
 
 
 function App() {
   
-
   return (
     <div className="App">
       <CollapsBtn/>
