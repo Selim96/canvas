2684786380b379fe2d41bbf539d23876a290cdf1
Selim Fazylov <selim.fazylov1@gmail.com>
2022-10-28 19:11:15 +0300

    multiple lines

--- src/App.js
+++ src/App.js
@@ -30,7 +30,45 @@ console.log(canvas)
 //   wrapper.append(stock);
 // }
 
+
+
+// let isStartDrawing = false;
+// let mouseStartPos = null;
+
+const mouse = {
+  isStartDrawing: false,
+  isDeleteLine: false,
+  mouseStartPos: null,
+  currentPos: null,
+
+  setStart(e, elem) {
+    this.isStartDrawing = !this.isStartDrawing;
+    this.mouseStartPos = this.getMousePosition(elem, e);
+  },
+
+  setStartFalse() {
+    this.isStartDrawing = false;
+  },
+
+  setCurrent(e, elem) {
+    this.currentPos = this.getMousePosition(elem, e);
+  },
+
+  getMousePosition(canvas, click) {
+    let rect = canvas.getBoundingClientRect();
+
+    const x = click.clientX - rect.left;
+    const y = click.clientY - rect.top;
+    return { x, y };
+  },
+
+  deleteLine() {
+    this.isDeleteLine = !this.isDeleteLine;
+  }
+};
+
 const ctx = canvas.getContext("2d");
+const lines = [];
 
 function drawLine(ctx, line) {
   const {start, end } = line;
@@ -41,17 +79,61 @@ function drawLine(ctx, line) {
   ctx.stroke();
 };
 
-let isStartDrawing = false;
-let mouseStartPos = null;
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height); 
+  lines.forEach(line => {
+    drawLine(ctx, line);
+  })
+};
 
-const mouse = {
-  isStartDrawing: false,
-  mouseStartPos: null,
-  currentPos: null,
-  setStart(e, elem) {
-    this.isStartDrawing = true
+function handleMouseClick(e) {
+  e.preventDefault();
+
+  if (e.button === 0) {
+    mouse.setStart(e, canvas);
+    mouse.deleteLine();
+  
+    const line = {
+      start: mouse.mouseStartPos,
+      end: mouse.mouseStartPos
+    };
+    
+    if (mouse.isStartDrawing) {
+      lines.push(line);
+      draw();
+    };
   }
-}
+
+  if (e.button === 2) {
+    mouse.setStartFalse();
+    if (mouse.isDeleteLine) {
+      mouse.deleteLine();
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+  }
+
+  if (e.button === 1) {
+    mouse.setStartFalse();
+  }
+};
+
+function handleMouseMove(e) {
+  if (mouse.isStartDrawing) {
+    mouse.setCurrent(e, canvas);
+
+    let line = {
+      start: mouse.mouseStartPos,
+      end: mouse.currentPos,
+    }
+    // ctx.clearRect(0, 0, canvas.width, canvas.height);
+    lines.pop();
+    lines.push(line);
+    draw();
+  }
+};
+
+canvas.addEventListener('mousedown', handleMouseClick);
+canvas.addEventListener('mousemove', handleMouseMove);
 
 function getMousePosition(click) {
   let rect = canvas.getBoundingClientRect();
@@ -59,42 +141,42 @@ function getMousePosition(click) {
   const x = click.clientX - rect.left;
   const y = click.clientY - rect.top;
   return {x, y};
-}
+};
 
-canvas.addEventListener('mousedown', (e) => {
-  e.preventDefault();
+// canvas.addEventListener('mousedown', (e) => {
+//   e.preventDefault();
   
-  mouseStartPos = getMousePosition(e);
+//   mouseStartPos = getMousePosition(e);
   
-  const line = {
-    start: mouseStartPos,
-    end: mouseStartPos
-  };
+//   const line = {
+//     start: mouseStartPos,
+//     end: mouseStartPos
+//   };
 
-  if (e.button === 0) {
-    isStartDrawing = !isStartDrawing;
-    if (isStartDrawing) {
-      drawLine(ctx, line);
-    }
-  } if (e.button === 2) {
-    isStartDrawing = false;
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-  } if (e.button === 1) {
-    isStartDrawing = false;
-  }
-});
+//   if (e.button === 0) {
+//     isStartDrawing = !isStartDrawing;
+//     if (isStartDrawing) {
+//       drawLine(ctx, line);
+//     }
+//   } if (e.button === 2) {
+//     isStartDrawing = false;
+//     ctx.clearRect(0, 0, canvas.width, canvas.height);
+//   } if (e.button === 1) {
+//     isStartDrawing = false;
+//   }
+// });
 
-canvas.addEventListener('mousemove', (e) => {
-  if (isStartDrawing) {
-    let line = {
-      start: mouseStartPos,
-      end: getMousePosition(e),
-    }
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+// canvas.addEventListener('mousemove', (e) => {
+//   if (isStartDrawing) {
+//     let line = {
+//       start: mouseStartPos,
+//       end: getMousePosition(e),
+//     }
+//     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    drawLine(ctx, line);
-  }
-});
+//     drawLine(ctx, line);
+//   }
+// });
 
 canvas.oncontextmenu = ((e) => {
   return false;
